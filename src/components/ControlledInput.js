33986984bea22e1f2ec8c8c0a0de1c4d068f7252
Dwire <gjdwyer8@gmail.com>
2018-05-18 00:58:53 -0400

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,22 @@
 // Code ControlledInput Component Here
+import React, {Component} from 'react'
+
+class ControlledInput extends Component {
+  state = {
+    value: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  render() {
+    return(
+      <input type="text" value={this.state.value} onChange={this.handleChange}/>
+    )
+  }
+}
+
+export default ControlledInput
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ControlledInput from './components/ControlledInput.js'
+
 ReactDOM.render(
-  <div>Mount Components Here</div>,
+  <div>
+    <h2>Mount Components Here</h2>
+    <ControlledInput />
+  </div>,
   document.getElementById('root')
 );
